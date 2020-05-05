467b89fa5079b310a4c32d2ba9437cdc7b568b06
Soufiane Salouf <soufiane.salouf@ibm.com>
2020-05-05 23:57:46 +0000

    init project

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Soufiane Salouf on 5/5/20.
+//  Copyright © 2020 Soufiane Salouf. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
